466d152abedc892adfda372343473897d9041dfc
ravhaim <github@ravhaim.org>
2023-08-28 20:37:14 +0300

    fix post

--- js/script.js
+++ js/script.js
@@ -145,7 +145,7 @@ $( document ). ready( function() {
                 return false
             }
             $( this ). button( 'loading' )
-            $.post( IFTTT, { contact: info }, null, 'json')
+            $.post( IFTTT, JSON.stringify({ contact: info }))
             .done( function( response ) {
                 $( '#site-contact-finish' ). modal( 'show' )
                 $( '.site-contact-success' ). show()
